5ed12dfd32427c895fdb7596d8fbbd815f473872
Dmitrii Okhlopkov <91318266+tmokkuss@users.noreply.github.com>
2023-04-06 18:02:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import t
+from scipy.stats import norm
 
 
 chat_id = 861665812 
 
 def solution(p: float, x: np.array) -> tuple:
-    a = 0.095
+    alpha = 1 - p
+    loc = x.mean()
+    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+    
     n = len(x)
-    x_max = x.max()
-    alpha = (1 - p)**(1./n)
-    left = x_max
-    right= (x_max - a) / alpha + a
+    s2=np.var(x, ddof=1)
+    q1 = chi2.ppf(p/2, df = n -1)
+    q2 = chi2.ppf(alpha/2, df = n-1)
+    left = np.sqrt((n-1)*s2/q2)/np.sqrt(48)
+    right =  np.sqrt((n-1)*s2/q1)/np.sqrt(48)
+    
     return (left, right)
